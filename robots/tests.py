7ce41deb06991451da2742f2fb8a0a398f0cd30c
Maria Chashchina <mchashchina@gmail.com>
2023-02-06 12:26:15 +0300

    Tested that DownloadWeeklyReportView generates correct queryset

--- robots/tests.py
+++ robots/tests.py
@@ -1,7 +1,11 @@
+import datetime
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
+from django.test import TestCase
 from rest_framework.test import APIClient, APITestCase
 from robots.models import Robot
+from robots.views import DownloadWeeklyReportView
+from openpyxl import Workbook
 
 
 class TestAPIPost(APITestCase):
@@ -35,3 +39,32 @@ class TestAPIPost(APITestCase):
             Robot.objects.get(model='R2')
         except ObjectDoesNotExist:
             assert True
+
+
+class TestWeeklyReport(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # Что мы ожидаем с этим набором тестовых данных:
+        # В отчёте 2 робота R2-D2, один R2-C3 и один C3-D2, R2-C5 не попадает вообще
+        time_data = [
+            datetime.datetime.now(),
+            datetime.datetime.now(),
+            datetime.datetime.now() - datetime.timedelta(days=8),
+            datetime.datetime.now() - datetime.timedelta(days=8),
+            datetime.datetime.now() - datetime.timedelta(days=7) - datetime.timedelta(hours=1),
+            datetime.datetime.now()
+        ]
+        Robot.objects.create(serial='R2-D2', model='R2', version='D2', created=time_data[0])
+        Robot.objects.create(serial='R2-C3', model='R2', version='C3', created=time_data[1]),
+        Robot.objects.create(serial='R2-C5', model='R2', version='C5', created=time_data[2]),
+        Robot.objects.create(serial='R2-D2', model='R2', version='D2', created=time_data[3])
+        Robot.objects.create(serial='R2-D2', model='R2', version='D2', created=time_data[4])
+        Robot.objects.create(serial='C3-D2', model='C3', version='D2', created=time_data[5])
+
+    def test_correct_queryset(self):
+        robot_1 = Robot.objects.get(serial='R2-C5')
+        robot_2 = Robot.objects.get(serial='C3-D2')
+        queryset = DownloadWeeklyReportView.get_last_week_production()
+        self.assertNotIn(robot_1, queryset)
+        self.assertIn(robot_2, queryset)
--- robots/views.py
+++ robots/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+import datetime
+from django.http import HttpResponse
+from openpyxl import Workbook
+from django.views import View
+from .models import Robot
 
-# Create your views here.
+
+class DownloadWeeklyReportView(View):
+
+    @staticmethod
+    def get_last_week_production():
+        # Немного шаманства, чтобы считались все данные за день неделю назад, а не только те, что были получены
+        # строго за последние 168 часов
+        # Не знаю, нужно ли исключать текущий день. В этой версии кода он не исключается из выдачи
+        today_date = datetime.date.today()
+        time = datetime.datetime.strptime('00:00:00', '%H:%M:%S').time()
+        today_full_date = datetime.datetime.combine(today_date, time)
+        week_ago = today_full_date - datetime.timedelta(days=7)
+        queryset = Robot.objects.filter(created__gte=week_ago)
+        return queryset
+
