df5ec6cfa7186f31f31e64426808b823bdb76688
Maria Chashchina <mchashchina@gmail.com>
2023-02-06 12:45:47 +0300

    Tested other email stuff - correct content, multiple recievers, deleting orders from db, etc.

--- orders/tests.py
+++ orders/tests.py
@@ -29,6 +29,37 @@ class TestOrdersApi(TestCase):
         Robot.objects.create(**robot_data)
         self.assertEqual(len(mail.outbox), 0)
 
+    def test_correct_email_content(self):
+        robot_data = {'serial': 'R2-D2', 'model': "R2", 'version': 'D2', 'created': '2022-12-31 23:59:59'}
+        Robot.objects.create(**robot_data)
+        self.assertEqual(mail.outbox[0].subject, "Робот R2-D2 теперь в наличии!")
+
+    def test_multiple_emails_sent(self):
+        customer = Customer.objects.create(**{'email': 'customer_2@customer.ru'})
+        Order.objects.create(**{'customer': customer, 'robot_serial': 'R2-D2'})
+        robot_data = {'serial': 'R2-D2', 'model': "R2", 'version': 'D2', 'created': '2022-12-31 23:59:59'}
+        Robot.objects.create(**robot_data)
+        self.assertEqual(len(mail.outbox), 2)
+
+    def test_proper_order_deleted(self):
+        order_1 = Order.objects.get(robot_serial='R2-D2')
+        order_2 = Order.objects.get(robot_serial='R2-C3')
+        robot_data = {'serial': 'R2-D2', 'model': "R2", 'version': 'D2', 'created': '2022-12-31 23:59:59'}
+        Robot.objects.create(**robot_data)
+        orders = Order.objects.all()
+        self.assertNotIn(order_1, orders)
+        self.assertIn(order_2, orders)
+
+    def test_multiple_orders_deleted(self):
+        customer = Customer.objects.create(**{'email': 'customer_2@customer.ru'})
+        Order.objects.create(**{'customer': customer, 'robot_serial': 'R2-D2'})
+        orders = Order.objects.filter(robot_serial='R2-D2')
+        robot_data = {'serial': 'R2-D2', 'model': "R2", 'version': 'D2', 'created': '2022-12-31 23:59:59'}
+        Robot.objects.create(**robot_data)
+        all_orders = Order.objects.all()
+        for order in orders:
+            self.assertNotIn(order, all_orders)
+
 
 
 
